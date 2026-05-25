892820a65d9f046001e469d3fbbc3f9a03752a2b
sailent29 <gsailent29@gmail.com>
2026-05-25 14:55:54 +0300

    path update again

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ if ("serviceWorker" in navigator) {
       const reg = await navigator.serviceWorker.register("/pwa-cheatsheet/sw.js", {
         scope: "/pwa-cheatsheet/"
       });
+
       console.log("SW registered:", reg.scope);
     } catch (e) {
       console.error("SW registration failed:", e);
